abc4f08eedfef427cfcfb96f11bae625bdb2ec6c
Dominick Trevino <domstrevino@aol.com>
2026-04-12 02:28:47 -0600

    change feature name to Secure Vault

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import Login from './pages/Login'
-import Passwords from './pages/Passwords'
 import Calendar from './pages/Calendar'
 import Tasks from './pages/Tasks'
 import Dashboard from './pages/Dashboard'
+import SecureVault from './pages/SecureVault'
 
 function App(): React.JSX.Element {
   return (
@@ -13,7 +13,7 @@ function App(): React.JSX.Element {
         <Route index element={<Dashboard />} />
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/passwords" element={<Passwords />} />
+        <Route path="/secure-vault" element={<SecureVault />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
---
+++ src/renderer/src/pages/SecureVault.tsx
@@ -0,0 +1,9 @@
+function SecureVault(): React.JSX.Element {
+  return (
+    <div>
+      <h1>Secure Vault</h1>
+    </div>
+  )
+}
+
+export default SecureVault
