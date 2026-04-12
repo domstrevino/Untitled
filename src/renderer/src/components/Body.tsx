b9fe4eaa910236e296def8ac4640c0f29ecf0839
Dominick Trevino <domstrevino@aol.com>
2026-04-11 18:02:41 -0600

    clean inline styles

--- src/renderer/src/components/Body.tsx
+++ src/renderer/src/components/Body.tsx
@@ -1,7 +1,5 @@
 function Body(): React.JSX.Element {
-  return (
-    <div style={{ border: '1px solid blue', height: '100%', flex: 1, display: 'flex' }}>Body</div>
-  )
+  return <div style={{ height: '100%', display: 'flex', flex: 1 }}>Body</div>
 }
 
 export default Body
